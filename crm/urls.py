bb5beb17c3ef64b41e10ad020c06ca8c6dc77757
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2025-05-19 14:05:49 +0000

    Split into base.html

--- crm/urls.py
+++ crm/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('contacts/<str:contact_id>/photo/', views.contact_photo, name='contact_photo'),
-    path('contacts/<str:contact_id>/', views.contact_detail, name='contact_detail'),
-    path('contacts/', views.show_contacts, name='show_contacts'),  # URL to display contacts
+    path('/<str:contact_id>/', views.contact_detail, name='contact_detail'),
+    path('', views.show_contacts, name='show_contacts'),  # URL to display contacts
     path('login/', views.contact_login, name='contact_login'),
 ]
