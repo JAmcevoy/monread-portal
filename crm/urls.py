7b25bff11c32be80af047d6dd69a12202ddeb43f
Joshua McEvoy <144149195+JAmcevoy@users.noreply.github.com>
2025-05-20 08:45:16 +0000

    change style of contact list and added a button in the nav to direct to list for staff users

--- crm/urls.py
+++ crm/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('contacts/<str:contact_id>/photo/', views.contact_photo, name='contact_photo'),
     path('/<str:contact_id>/', views.contact_detail, name='contact_detail'),
-    path('', views.show_contacts, name='show_contacts'),  # URL to display contacts
+    path('', views.show_contacts, name='contacts'),  # URL to display contacts
     path('login/', views.contact_login, name='contact_login'),
 ]
